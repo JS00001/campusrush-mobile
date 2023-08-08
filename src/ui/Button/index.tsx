c70852cc0cd3237942f72cd002878927e3eba424
JS00001 <senyitkojack@gmail.com>
2023-08-07 21:59:17 -0400

    UI Components

---
+++ src/ui/Button/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * Created on Mon Aug 07 2023
+ *
+ * This software is the proprietary property of CampusRush.
+ * All rights reserved. Unauthorized copying, modification, or distribution
+ * of this software, in whole or in part, is strictly prohibited.
+ * For licensing information contact CampusRush.
+ *
+ * Copyright (c) 2023 CampusRush
+ * Do not distribute
+ */
+
+import Button from "./Button";
+
+export default Button;
---
+++ src/ui/SegmentedControl/SegmentedControl.tsx
@@ -0,0 +1,19 @@
+/*
+ * Created on Mon Aug 07 2023
+ *
+ * This software is the proprietary property of CampusRush.
+ * All rights reserved. Unauthorized copying, modification, or distribution
+ * of this software, in whole or in part, is strictly prohibited.
+ * For licensing information contact CampusRush.
+ *
+ * Copyright (c) 2023 CampusRush
+ * Do not distribute
+ */
+
+interface SegmentedControlProps {}
+
+const SegmentedControl: React.FC<SegmentedControlProps> = () => {
+  return <></>;
+};
+
+export default SegmentedControl;
