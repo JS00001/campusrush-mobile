e2e50a01f1592f28fb074e7316388e32c615094c
JS00001 <senyitkojack@gmail.com>
2023-08-23 19:12:43 -0400

    Add organizations option

---
+++ src/api/api/organization.ts
@@ -0,0 +1,19 @@
+/*
+ * Created on Fri Aug 11 2023
+ *
+ * This software is the proprietary property of CampusRush.
+ * All rights reserved. Unauthorized copying, modification, or distribution
+ * of this software, in whole or in part, is strictly prohibited.
+ * For licensing information contact CampusRush.
+ *
+ * Copyright (c) 2023 CampusRush
+ * Do not distribute
+ */
+
+import { axiosClient } from '@/providers/Axios';
+
+import { API_VERSION_URL } from '@/api/constants';
+
+axiosClient.defaults.baseURL = API_VERSION_URL + '/organization';
+
+const authAPIClient = axiosClient;
