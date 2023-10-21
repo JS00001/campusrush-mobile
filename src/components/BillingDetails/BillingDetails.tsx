64f97b9aaf4ebd3c5dad2651df7db6f369c78d57
JS00001 <senyitkojack@gmail.com>
2023-10-21 11:27:10 -0400

    fix key issues with billing

--- src/components/BillingDetails/BillingDetails.tsx
+++ src/components/BillingDetails/BillingDetails.tsx
@@ -31,6 +31,7 @@ const BillingDetails: React.FC<BillingDetailsProps> = ({ activeProducts }) => {
     <View style={tw`w-full`}>
       {activeProducts.map((product) => (
         <SelectionCard
+          key={product.title}
           pressable={false}
           title={product.title}
           subtitle={product.subtitle}
@@ -38,7 +39,7 @@ const BillingDetails: React.FC<BillingDetailsProps> = ({ activeProducts }) => {
         >
           {product.perks.length > 0 &&
             product.perks.map((perk) => (
-              <View style={tw`flex-row gap-2`}>
+              <View key={perk} style={tw`flex-row gap-2`}>
                 <RemixIcon
                   name="ri-check-line"
                   color={tw.color("primary")}
