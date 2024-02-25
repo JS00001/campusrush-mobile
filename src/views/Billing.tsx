07a382b3a2293cff1efab89d6bca404dbea507b1
JS00001 <senyitkojack@gmail.com>
2024-02-24 23:11:25 -0500

    Refactor admin screen & begin billing

--- src/views/Billing.tsx
+++ src/views/Billing.tsx
@@ -1,12 +1,12 @@
 /*
- * Created on Mon Aug 07 2023
+ * Created on Sat Feb 24 2024
  *
  * This software is the proprietary property of CampusRush.
  * All rights reserved. Unauthorized copying, modification, or distribution
  * of this software, in whole or in part, is strictly prohibited.
  * For licensing information contact CampusRush.
  *
- * Copyright (c) 2023 CampusRush
+ * Copyright (c) 2024 CampusRush
  * Do not distribute
  */
 
@@ -14,14 +14,13 @@ import { View } from "react-native";
 
 import Text from "@/ui/Text";
 import tw from "@/lib/tailwind";
-import Layout from "@/ui/Layout";
 import Button from "@/ui/Button";
 import Hyperlink from "@/ui/Hyperlink";
 import { useAuth } from "@/providers/Auth";
 import usePurchase from "@/hooksv1/usePurchase";
 import ProductCard from "@/components/ProductCard";
 
-const BillingScreen = () => {
+const BillingView = () => {
   const {
     packages,
     buttonCTA,
@@ -46,9 +45,7 @@ const BillingScreen = () => {
   if (areOfferingsLoading) return null;
 
   return (
-    <Layout scrollable gap={18}>
-      <Layout.Header title="Billing" subtitle="Select a plan to get started" />
-
+    <>
       {packages?.map(({ product }, i) => (
         <ProductCard
           key={i}
@@ -77,8 +74,8 @@ const BillingScreen = () => {
           </Hyperlink>
         </View>
       </View>
-    </Layout>
+    </>
   );
 };
 
-export default BillingScreen;
+export default BillingView;
