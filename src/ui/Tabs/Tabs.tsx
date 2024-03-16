39103188f98e14f8ae41cf834d33b925bc05d5ce
JS00001 <senyitkojack@gmail.com>
2024-03-15 20:58:22 -0400

    Add tabs

---
+++ src/ui/Tabs/Tabs.tsx
@@ -0,0 +1,69 @@
+/*
+ * Created on Fri Mar 15 2024
+ *
+ * This software is the proprietary property of CampusRush.
+ * All rights reserved. Unauthorized copying, modification, or distribution
+ * of this software, in whole or in part, is strictly prohibited.
+ * For licensing information contact CampusRush.
+ *
+ * Copyright (c) 2024 CampusRush
+ * Do not distribute
+ */
+
+import { View, ViewProps } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+import Tab from "./Tab";
+
+import tw from "@/lib/tailwind";
+
+interface TabsProps extends ViewProps {
+  options: string[];
+  currentIndex: number;
+  disabledIndex?: number[];
+  style?: any;
+  onChange: (index: number) => void;
+}
+
+const Tabs: React.FC<TabsProps> = ({
+  options,
+  currentIndex,
+  disabledIndex = [],
+  style,
+  onChange,
+  ...props
+}) => {
+  const containerStyles = tw.style("w-full", style);
+
+  const contentContainerStyles = tw.style("gap-1");
+
+  return (
+    <View style={containerStyles} {...props}>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={contentContainerStyles}
+      >
+        {options.map((option, index) => {
+          const isSelected = currentIndex === index;
+
+          const handlePress = () => {
+            onChange(index);
+          };
+
+          return (
+            <Tab
+              key={index}
+              label={option}
+              selected={isSelected}
+              disabled={disabledIndex.includes(index)}
+              onPress={handlePress}
+            />
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Tabs;
