78dc15fddfede45b29ce7eba322ef57dd1fe2246
JS <49812749+JS00001@users.noreply.github.com>
2024-07-24 21:07:00 -0400

    begin onboarding component

---
+++ src/components/Onboarding/Onboarding.tsx
@@ -0,0 +1,15 @@
+/*
+ * Created on Wed Jul 24 2024
+ *
+ * This software is the proprietary property of CampusRush.
+ * All rights reserved. Unauthorized copying, modification, or distribution
+ * of this software, in whole or in part, is strictly prohibited.
+ * For licensing information contact CampusRush.
+ *
+ * Copyright (c) 2024 CampusRush
+ * Do not distribute
+ */
+
+// Create an onboarding component that automatically shuffles the grid, sets the buttons, handles navigation, etc.
+// Also add a 'content' component that will be rendered in the cells
+// Also add title, description, number of steps, etc.
